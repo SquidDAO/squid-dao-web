f3f40678c0003bb92965d48da5b089fa5addbe65
Baron Reznik <482978+breznik@users.noreply.github.com>
2021-12-05 12:19:21 -0800

    Add Github logo/link to sidebar

--- src/components/Sidebar/Social.jsx
+++ src/components/Sidebar/Social.jsx
@@ -2,12 +2,15 @@ import { SvgIcon, Link } from "@material-ui/core";
 import { ReactComponent as Twitter } from "../../assets/icons/twitter.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { faDiscord, faReddit } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord, faGithub, faReddit } from "@fortawesome/free-brands-svg-icons";
 import styled from "styled-components";
 
 export default function Social() {
   return (
     <div className="social-row">
+      <Link href="https://github.com/squiddaodev" target="_blank">
+        <Icon icon={faGithub} size="1x" />
+      </Link>
       <Link href="https://twitter.com/SquidDao" target="_blank">
         <SvgIcon color="primary" component={Twitter} />
       </Link>
