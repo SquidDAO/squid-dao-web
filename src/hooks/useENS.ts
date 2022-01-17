8002ed7b7ff15f719eb0f939dfd48a6fd08964e5
Yoginth <yoginth.saravanan@cred.club>
2022-01-17 12:41:02 +0530

    chore: use useWeb3Context

--- src/hooks/useENS.ts
+++ src/hooks/useENS.ts
@@ -1,13 +1,13 @@
-import { getDefaultProvider } from "@ethersproject/providers";
 import { useEffect, useState } from "react";
+import { useWeb3Context } from "./web3Context";
 
 export function useENS(address: string | null | undefined) {
   const [ensName, setENSName] = useState<string | null>();
+  const { provider } = useWeb3Context();
 
   useEffect(() => {
     async function resolveENS() {
       if (address) {
-        const provider = await getDefaultProvider();
         const name = await provider.lookupAddress(address.toLowerCase());
         if (name) setENSName(name);
       }
